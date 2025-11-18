c1adcaf19faafdcc3e6bd56083a0a575ab4dcdac
ry <ry@dummy.com>
2025-11-18 08:30:35 +0700

    install spatie

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    App\Providers\EventServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
 ];
