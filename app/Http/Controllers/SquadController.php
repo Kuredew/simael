73542c3f2a6bb8690dec6a62e9b4282594cf61f1
Kuredew <burrohmanhabib35@gmail.com>
2025-11-19 21:41:13 +0700

    feat: add squad model, migration and controller

---
+++ app/Http/Controllers/SquadController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Squad;
+use Illuminate\Http\Request;
+
+class SquadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Squad $squad)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Squad $squad)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Squad $squad)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Squad $squad)
+    {
+        //
+    }
+}
