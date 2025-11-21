0cf1dffd25325e6276fba967c2f3ca0af12ef109
Kuredew <burrohmanhabib35@gmail.com>
2025-11-21 21:04:16 +0700

    chore(app): Change font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,14 +10,14 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Stack+Sans+Headline:wght@200..700&display=swap" rel="stylesheet">
     
     <!-- Styles -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     
     <style>
         body {
-            font-family: 'Inter', sans-serif;
+            font-family: 'Stack Sans Headline', sans-serif;
         }
     </style>
     
